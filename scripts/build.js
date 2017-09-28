c666a7384fbb56f48a7324b88b3c15e7aad6cafe
Jonathan Giroux <giroux.jo@gmail.com>
2017-09-29 01:05:36 +0200

    Move into static

--- scripts/build.js
+++ scripts/build.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const { readdirSync } = require('fs');
 const { bundleSFX } = require('jspm');
 const { join, resolve } = require('path');
 const copyFile = require('quickly-copy-file');
@@ -13,8 +14,14 @@ Promise.all([
 	rimraf(join(buildPath, '*')),
 ])
 	.then(() => {
-		console.log('Copying files');
+		console.log('Copying index');
 		return copyFile(join(rootPath, 'browser', 'index-release.html'), join(buildPath, 'index.html'));
+	})
+	.then(() => {
+		console.log('Copying static files');
+		const staticDirectory = join(rootPath, 'browser', 'static');
+		const files = readdirSync(staticDirectory);
+		return Promise.all(files.map(file => copyFile(join(staticDirectory, file), join(buildPath, 'static', file))));
 	})
 	.then(() => {
 		console.log('Bundling JSPM app');
