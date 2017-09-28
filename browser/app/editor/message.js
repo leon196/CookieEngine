7a0b17d07400238ad97fe150ce55e798774314b3
Leon <lowpolygonum@gmail.com>
2017-09-29 00:47:28 +0200

    tweaking

--- browser/app/editor/message.js
+++ browser/app/editor/message.js
@@ -13,7 +13,7 @@ export var message =
 		,'the crowd gathered at'
 		,'COOKIE DEMOPARTY'
 		,'8-9 December 2017\nParis France'
-		,'Thanksfully the beers were still cold.'
+		,'Thankfully the beers were still cold.'
 		,'greets to\nLJ Raxter Flush\nResistance X-Men\nUnix Calodox\n'
 		,'we love\nmercury still\nasd cocoon razor1911\nhoodlum paradox'
 		,'credits\nartcode ponk\ncode koltes\nmusic fergus'
