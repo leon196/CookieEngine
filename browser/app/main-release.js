5bc814328407c9462f7683348f55dbbd21fe23b4
Leon <lowpolygonum@gmail.com>
2017-10-01 20:30:25 +0200

    clean

--- browser/app/main-release.js
+++ browser/app/main-release.js
@@ -1,17 +1,3 @@
 import demo from './demo';
 
-let started = false;
-
-function start(event) {
-	event.preventDefault();
-
-	if (!started) {
-		started = true;
-
-		document.getElementById('message').remove();
-
-		setTimeout(demo, 0);
-	}
-}
-
-addEventListener('click', start);
+demo();
